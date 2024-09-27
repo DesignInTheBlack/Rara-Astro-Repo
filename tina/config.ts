68a59f823f2b0659ab9f41ce9c053ae9a599a16f
Mr. Black <37428879+DesignInTheBlack@users.noreply.github.com>
2024-09-27 13:40:21 -0500

    Structuring Single Page Format for Tina CMS

---
+++ tina/config.ts
@@ -0,0 +1,80 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+
+  schema: {
+    collections: [
+      {
+        label: 'Homepage',
+        name: 'homepage',
+        path: 'content',
+        match: {
+          include: 'homepage',
+        },
+        fields: [
+          {
+            type: 'string',
+            label: 'Page Title',
+            name: 'title',
+          },
+          // Add more fields as needed
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+        },
+      },
+      {
+        label: 'About',
+        name: 'about',
+        path: 'content',
+        match: {
+          include: 'about',
+        },
+        fields: [
+          {
+            type: 'string',
+            label: 'Page Title',
+            name: 'title',
+          },
+          // Add more fields as needed
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+        },
+      },
+    ],
+  },
+
+
+});
