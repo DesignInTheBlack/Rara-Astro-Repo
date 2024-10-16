dd84c994dcd2e1f5b63ade87984f0e5bd3343e4c
Mr. Black <37428879+DesignInTheBlack@users.noreply.github.com>
2024-10-15 20:55:34 -0500

    Adjust Padding On Buffer

--- elevate/design/spacing.js
+++ elevate/design/spacing.js
@@ -4,9 +4,9 @@ module.exports = {
 
   // Buffer padding references
   bufferPadding: {
-    '2xs': 'd6',
-    'xs': 'd6',
-    'sm': 'd6',
+    '2xs': 'd4',
+    'xs': 'd4',
+    'sm': 'd4',
     'md': 'd8',
     'lg': 'd8',
     'xl': 'd8',
