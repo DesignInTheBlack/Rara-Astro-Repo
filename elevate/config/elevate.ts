3311c900a9399307186259be02683985006f7117
Mr. Black <37428879+DesignInTheBlack@users.noreply.github.com>
2025-02-06 19:11:18 -0600

    Further Updates

--- elevate/config/elevate.ts
+++ elevate/config/elevate.ts
@@ -2,7 +2,7 @@ const options = {
     Watch: './src', // Folder to watch for changes
     FileTypes: ['astro'], // Valid filetypes to watch for changes
     Output: './src/styles', // Where to put the compiled CSS
-    Extend: ['./elevate/ext/fonts.css'], // CSS files to include as well
+    Extend: ['./elevate/ext/fonts.css', './elevate/ext/texture.css'], // CSS files to include as well
     ClassRegex: [
         /class\s*=\s*"([^"]+)"/g, // Matches class="..."
         /componentClass\s*=\s*\{'([^']+)'\}/g, // Matches componentClass={'...'}
